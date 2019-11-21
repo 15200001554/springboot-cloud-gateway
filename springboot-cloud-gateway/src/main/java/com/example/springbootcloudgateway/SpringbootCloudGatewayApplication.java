c9aded37ec28005d23e2634f3f6075715634d719
zhaonian <niannian3302@163.com>
2019-11-21 19:58:37 +0800

    第二次提交

--- springboot-cloud-gateway/src/main/java/com/example/springbootcloudgateway/SpringbootCloudGatewayApplication.java
+++ springboot-cloud-gateway/src/main/java/com/example/springbootcloudgateway/SpringbootCloudGatewayApplication.java
@@ -18,10 +18,6 @@ public class SpringbootCloudGatewayApplication {
                 .route(p -> p
                         .path("/get")
                         .filters(f -> f.addRequestHeader("Hello", "World"))
-                        .uri("http://httpbin.org:80"))
-                .route(p -> p
-                        .host("*.hystrix.com")
-                        .filters(f -> f.hystrix(config -> config.setName("mycmd")))
                         .uri("http://httpbin.org:80")).
                         build();
     }
